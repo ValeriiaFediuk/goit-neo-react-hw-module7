85a0e291f3c53f96924bf79888c09fa2472421e1
ValeriiaFediuk <fediuk.valeriia14@gmail.com>
2024-10-20 20:55:31 +0200

    second trial

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts } from '../../redux/contactsSlice';
-import { selectNameFilter } from '../../redux/filtersSlice';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 import { deleteContact } from '../../redux/contactsOps';
 import styles from './ContactList.module.css';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
+  const contacts = useSelector(selectFilteredContacts);
   const dispatch = useDispatch();
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  if (filteredContacts.length === 0) {
-    return <p>No contacts found</p>;
-  }
 
   return (
     <ul className={styles.list}>
-      {filteredContacts.map((contact) => (
-        <li key={contact.id} className={styles.item}>
+      {contacts.map((contact) => (
+        <li key={contact.id}>
           {contact.name}: {contact.number}
-          <button
-            className={styles.button}
-            onClick={() => dispatch(deleteContact(contact.id))}
-          >
+          <button onClick={() => dispatch(deleteContact(contact.id))}>
             Delete
           </button>
         </li>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,37 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchContacts, addContact, deleteContact } from './contactsOps';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
+import { selectNameFilter } from './filtersSlice';
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [],
-    loading: false,
-    error: null,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchContacts.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.loading = false;
+      .addCase('contacts/fetchContacts/fulfilled', (state, action) => {
         state.items = action.payload;
       })
-      .addCase(fetchContacts.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message;
-      })
-      .addCase(addContact.fulfilled, (state, action) => {
+      .addCase('contacts/addContact/fulfilled', (state, action) => {
         state.items.push(action.payload);
       })
-      .addCase(deleteContact.fulfilled, (state, action) => {
+      .addCase('contacts/deleteContact/fulfilled', (state, action) => {
         state.items = state.items.filter(contact => contact.id !== action.payload);
       });
   },
 });
 
-export const selectContacts = (state) => state.contacts.items;
+export const selectContacts = state => state.contacts.items;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
 
 export default contactsSlice.reducer;
